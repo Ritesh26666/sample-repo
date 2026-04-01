735e44a45cb796ece6dc8bcca11c3ff243c221ae
Ritesh Yadav <riteshyadav21314@gmail.com>
2026-04-01 20:51:07 +0530

    Add files via upload

---
+++ stringprintsubstring.java
@@ -0,0 +1,11 @@
+public class stringprintsubstring {
+    public static void main(String[] args) {
+        String s = "abcd";
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j<s.length()-1;j++){
+                
+            }
+            System.out.println();
+        }
+    }
+}
